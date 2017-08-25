736ef4b36d3972032248cc538d38698f24e3b67b
dengxy <dengxy@YFB-DENGXY.sumpay.local>
2017-08-25 15:04:20 +0800

    update

---
+++ src/main/resource/test.sql
@@ -0,0 +1,66 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.5.28 : Database - test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `test`;
+
+/*Table structure for table `accout` */
+
+DROP TABLE IF EXISTS `accout`;
+
+CREATE TABLE `accout` (
+  `usernanme` varchar(20) DEFAULT NULL,
+  `introduce` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `accout` */
+
+/*Table structure for table `pictures` */
+
+DROP TABLE IF EXISTS `pictures`;
+
+CREATE TABLE `pictures` (
+  `picturesID` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(40) DEFAULT NULL,
+  `imagepath` varchar(40) DEFAULT NULL,
+  `ID` int(11) DEFAULT NULL,
+  `count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`picturesID`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+/*Data for the table `pictures` */
+
+insert  into `pictures`(`picturesID`,`type`,`imagepath`,`ID`,`count`) values (1,'A','test/a1.jpg',NULL,NULL),(12,'A','test/a2.jpg',NULL,NULL),(13,'A','test/a3.jpeg',NULL,NULL),(14,'A','test/a4.jpg',NULL,NULL),(18,'A','test/a5.jpg',NULL,NULL),(19,'A','test/a6.jpg',NULL,NULL),(20,'A','test/a7.jpg',NULL,NULL);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `type` varchar(1) DEFAULT '0',
+  `ID` int(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`username`,`password`,`type`,`ID`) values ('123456','132','1',2),('a','123','1',3),('15168369180@163.com','1234','1',9),('1358134297@qq.com','123','1',10);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
